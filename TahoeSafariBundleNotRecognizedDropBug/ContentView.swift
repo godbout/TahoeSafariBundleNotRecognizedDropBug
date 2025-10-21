2fecb886202b7209ad018a64cb7e3ee27ce2ba87
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2025-10-21 14:31:43 +0800

    add drop feature that breaks under Tahoe

--- TahoeSafariBundleNotRecognizedDropBug/ContentView.swift
+++ TahoeSafariBundleNotRecognizedDropBug/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ContentView: View {
     var body: some View {
@@ -13,12 +14,67 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+                .onDrop(of: [.fileURL], delegate: AppsDropDelegate())
             Text("Hello, world!")
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+private struct AppsDropDelegate: DropDelegate {
+
+    func validateDrop(info: DropInfo) -> Bool {
+        guard info.hasItemsConforming(to: [.fileURL]) else { return false }
+
+        let providers = info.itemProviders(for: [.fileURL])
+        var result = false
+
+        for provider in providers {
+            if provider.canLoadObject(ofClass: URL.self) {
+                let group = DispatchGroup()
+                group.enter()
+
+                _ = provider.loadObject(ofClass: URL.self) { url, _ in
+                    let itemIsAnApplicationBundle = try? url?.resourceValues(forKeys: [.contentTypeKey]).contentType == .applicationBundle
+                    result = result || (itemIsAnApplicationBundle ?? false)    
+                    group.leave()
+                }
+                                
+                _ = group.wait(timeout: .now() + 0.5)
+            }
+        }
+
+        return result
+    }
+
+    func performDrop(info: DropInfo) -> Bool {
+        let providers = info.itemProviders(for: [.fileURL])
+        var result = false
+
+        for provider in providers {
+            if provider.canLoadObject(ofClass: URL.self) {
+                let group = DispatchGroup()
+                group.enter()
+
+                _ = provider.loadObject(ofClass: URL.self) { url, _ in
+                    let itemIsAnApplicationBundle = (try? url?.resourceValues(forKeys: [.contentTypeKey]).contentType == .applicationBundle) ?? false
+                    
+                    if itemIsAnApplicationBundle {
+                        DispatchQueue.main.async {
+                            print("dropped")
+                        }
+                        
+                        result = result || true
+                    }
+                                        
+                    group.leave()
+                }
+
+                _ = group.wait(timeout: .now() + 0.5)
+            }
+        }
+        
+        return result
+    }
+    
 }
