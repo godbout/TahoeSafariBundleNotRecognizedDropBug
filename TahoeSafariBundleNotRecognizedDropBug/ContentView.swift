1b7683c90c40c8a42756c5bbeb09b7700824e877
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2025-10-21 14:23:14 +0800

    xcode init innit

---
+++ TahoeSafariBundleNotRecognizedDropBug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TahoeSafariBundleNotRecognizedDropBug
+//
+//  Created by Guillaume Leclerc on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TahoeSafariBundleNotRecognizedDropBug/TahoeSafariBundleNotRecognizedDropBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahoeSafariBundleNotRecognizedDropBugApp.swift
+//  TahoeSafariBundleNotRecognizedDropBug
+//
+//  Created by Guillaume Leclerc on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TahoeSafariBundleNotRecognizedDropBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
